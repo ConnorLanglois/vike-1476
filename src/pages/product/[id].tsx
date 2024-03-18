3bd8e8a345f3c6e5f8230db42e665c5897ae1b74
Connor <connor.langlois@gmail.com>
2024-03-18 16:56:36 -0400

    Init commit

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,13 +1,16 @@
 import Link from "next/link";
+import { useRouter } from 'next/router';
 import { Inter } from "next/font/google";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  const { asPath } = useRouter();
   return (
     <main
       className={`flex min-h-screen flex-col items-center p-24 ${inter.className}`}
     >
+      Welcome to {asPath}
       {new Array(10).fill(undefined).map((_, i) => (
         <Link key={i} href={`/product/${i}`}>Go to /product/{i}</Link>
       ))}
